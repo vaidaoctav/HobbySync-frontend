5338ec63ff6ee2727b490ef1b70bbab11912f24f
vaidaoctav <octav.vaida@stud.ubbcluj.ro>
2024-04-11 15:17:35 +0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+
+const vuetify = createVuetify({
+    components,
+    directives
+  })
+
+app.use(createPinia())
+app.use(router)
+app.use(vuetify)
+
+app.mount('#app')
