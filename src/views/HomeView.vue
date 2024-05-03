58b133e5be69e34a75b7abdf66d763e00273c3d6
vaidaoctav <octav.vaida@stud.ubbcluj.ro>
2024-05-03 18:44:29 +0300

    Home and Login pages

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,135 @@
-<script setup>
+<template>
+  <div class="containment">
+    <div class="content-overlay">
+      <nav class="navigation">
+        <button @click="goToLogin">Login</button>
+        <button @click="goToRegister">Register</button>
+        <button @click="goToAboutUs">About Us</button>
+      </nav>
+    </div>
+    <div style="display: flex; flex-direction: row;">
+      <header class="header">
+        <!-- <h1>HobbySync</h1> -->
+        <img
+        src="../assets/hobbysync.png"
+        width="600px"
+        height="150px"
+        style="align-self: center; margin-bottom: 75px; margin-top: 50px;">
+        <p>Connect with local events and communities that share your passions. Discover, engage, and expand your hobbies with HobbySync. Join the fun, meet new friends, and explore your interests with ease.</p>
+      </header>
+      <v-spacer></v-spacer>
+      <img
+      src="../assets/Home.jpg"
+      width="500px"
+      height="500px"
+      style="border-radius: 100px; margin-right: 125px; margin-top: 75px;">
+    </div>
+  </div>
+</template>
 
+<script>
+export default {
+  name: 'HomeView',
+  methods: {
+    goToLogin() {
+      this.$router.push({ name: 'login' });
+    },
+    goToRegister() {
+      this.$router.push({ name: 'register' });
+    },
+    goToAboutUs() {
+      this.$router.push({ name: 'about' });
+    },
+    goToExplore() {
+      this.$router.push({ name: 'explore' });
+    },
+    goToEvents() {
+      this.$router.push({ name: 'events' });
+    },
+    goToContact() {
+      this.$router.push({ name: 'contact' });
+    }
+  }
+}
 </script>
 
-<template>
-  <div>Hello</div>>
-</template>
+<style scoped>
+.home-container {
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  color: white;
+  font-family: "Platypi", serif;
+}
+
+.containment {
+        position: relative;
+        width: 100vw;
+        height: 100vh;
+        background-image: url("../assets/Background.jpg");
+        background-position: center;
+        background-size: cover;
+        margin-left: -50vw;
+        margin-right: -50vw;
+        left: 50%;
+        right: 50%;
+    }
+
+.content-overlay {
+  background-color: rgba(0, 0, 50, 0.6); 
+  padding: 15px;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.5);
+}
+
+.header {
+  margin-left: 50px;
+  display: flex;
+  flex-direction: column;
+}
+.header h1 {
+  font-size: 3rem;
+  margin: 0;
+  margin-bottom: 75px;
+  margin-top: 100px;
+  text-align: center;
+  font-family: "Platypi", serif;
+        font-weight: 700;
+        font-size: xx-large;
+        font-style: normal;
+        color: white;
+}
+
+.header p {
+  font-size: 1.4rem;
+  margin-top: 0.5rem;
+  margin-bottom: 2rem;
+  max-width: 600px;
+  text-align: center;
+  font-family: "Platypi", serif;
+        font-weight: 400;
+        font-size: x-large;
+        font-style: normal;
+        color: white;
+}
+
+.navigation button {
+  background: transparent;
+  border: 2px solid white;
+  color: white;
+  padding: 10px 20px;
+  margin: 5px;
+  font-size: 1rem;
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
+
+.navigation button:hover {
+  background-color: white;
+  color: #003366;
+}
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,193 @@
+<template>
+    <div class="containment">
+        <router-link to="/" style="text-decoration: none;">
+            <v-btn
+                icon="mdi-home"
+                size="x-large"
+                variant="elevated"
+                color="#015986"
+                style="margin-top: 660px; margin-left: 15px;"
+            >
+            </v-btn>
+        </router-link>
+
+        <div class="login-form">
+
+            <img src="@/assets/Logo.png"
+                width="125px"
+                height="125px"
+                style="margin-bottom: 10px; border-radius: 100px;">
+
+            <v-card class="content">
+                <div class="title">Login</div>
+
+                <div class="input-type">Username:</div>
+
+                <v-text-field
+                    prepend-inner-icon="mdi-account"
+                    variant="solo" 
+                    class="input"
+                    density="compact"
+                    rounded
+                ></v-text-field>
+
+                <div class="input-type">Password:</div>
+
+                <v-text-field
+                    :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+                    :type="visible ? 'text' : 'password'"
+                    prepend-inner-icon="mdi-lock-outline"
+                    variant="solo"
+                    density="compact"
+                    class="input"
+                    rounded
+                    @click:append-inner="visible = !visible"
+                ></v-text-field>
+
+                <v-btn 
+                    size="large" 
+                    rounded="xl"
+                    class="login-button"
+                >
+                    LOGIN
+                </v-btn>
+
+                <v-btn
+                    color="white"
+                    variant="text"
+                    class="link-button"
+                >
+                    Forgot your password?
+                </v-btn>
+            </v-card>
+
+            <div style="display: flex; flex-direction: row;">                
+                <router-link to="/register" style="text-decoration: none;">
+                    <v-btn
+                    variant="text"
+                    class="register-button"
+                    >
+                        Register now!
+                    </v-btn>
+                </router-link>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            visible: false
+        })
+    }
+</script>
+
+<style scoped>
+    .containment {
+        position: relative;
+        width: 100vw;
+        height: 100vh;
+        background-image: url("../assets/Background.jpg");
+        background-size: cover;
+        margin-left: -50vw;
+        margin-right: -50vw;
+        left: 50%;
+        right: 50%;
+    }
+
+    .login-form {
+        margin-top: 30px;
+        position: absolute;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 450px;
+        height: 700px;
+    }
+
+    .content {
+        width: 450px;
+        height: 500px;
+        background-color: #7d0d50;
+        border-radius: 60px;
+        border: 6px solid white;
+        margin-left: auto;
+        margin-right: auto;
+        padding: 15px;
+        padding-bottom: 8px;
+    }
+
+    .title {
+        margin: 25px auto 0 auto;
+        font-family: "Platypi", serif;
+        font-weight: 400;
+        font-size: xx-large;
+        font-style: normal;
+        color: white;
+        text-align: center;
+    }
+
+    .input-type {
+        margin-left: 10px;
+        margin-bottom: 5px;
+        margin-top: 30px;
+        font-family: "Platypi", serif;
+        font-weight: 400;
+        font-size: medium;
+        font-style: normal;
+        color: white;
+    }
+
+    .input {
+        width: 410px;
+        height: 30px;
+        margin-bottom: 25px;
+    }
+
+    .login-button {
+      font-family: "Platypi", serif;
+      font-weight: 400;
+      font-style: normal;
+      background-color: rgba(0,88,131,255);
+      color:white;
+      width: 200px;
+      margin-top: 30px;
+      margin-left: 104px;
+    }
+
+    .link-button {
+      font-family: "Platypi", serif;
+      font-weight: 400;
+      font-style: normal;
+      font-size: medium; 
+      justify-content: center;
+      margin-top: 70px;
+      margin-left: 79px;
+      text-transform: unset !important;
+      width: 250px;
+    }
+
+    .register-button {
+      font-family: "Platypi", serif;
+      font-weight: 400;
+      font-style: normal;
+      font-size: medium;
+      color:rgba(0,88,131,255);
+      text-transform: unset !important;
+      margin-left: 1px;
+    }
+
+    .text {
+        color: #7d2e2e;
+        font-family: "Platypi", serif;
+        font-weight: 400;
+        font-style: normal;
+        font-size: medium;
+        margin-top: 6px;
+    }
+</style>
